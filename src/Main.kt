8fab69b6c334d3941774a49fd362aa1c0e44da95
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-10-31 12:10:47 -0400

    Finished Assignment 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,2 +1,11 @@
-class Main {
+fun main() {
+    val buildMaterials = mutableMapOf(
+        "Sediment" to "Dirt",
+        "Rock" to "Stone",
+        "Liquid" to "Lava"
+    )
+    buildMaterials["Woods"] = "Spruce"
+    buildMaterials["Liquid"] = "Water"
+    buildMaterials.remove("Sediment")
+    println(buildMaterials)
 }
